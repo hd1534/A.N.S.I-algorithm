b5c92bd1ede251116c9a95e3dab6f7ddcec65077
hd3630 <nahjh2@naver.com>
2021-04-25 00:09:30 +0900

    Update 하노이 탑 이동 순서.py

--- 개인적으로 푼 문제/하노이 탑 이동 순서/하노이 탑 이동 순서.py
+++ 개인적으로 푼 문제/하노이 탑 이동 순서/하노이 탑 이동 순서.py
@@ -17,11 +17,9 @@ def sol():
     hanoi(n, 1, 3, 2)
 
 
-def short():
-    def h(n, f, t): n and (h(n-1, f, f ^ t), print(f, t), h(n-1, f ^ t, t))
-    n = int(input())
-    print(2**n-1)
-    h(n, 1, 3)
+def short(): # 94글자
+    def h(n,f,t):n and(h(n-1,f,f^t),print(f,t),h(n-1,f^t,t))
+    n=int(input());print(2**n-1);h(n,1,3)
 
 
 if __name__ == "__main__":
