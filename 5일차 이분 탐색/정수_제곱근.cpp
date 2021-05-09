a9f73f1271db790b33886e6ce23b733fd5d96a18
nahjh2 <nahjh2@gmail.com>
2021-05-09 23:35:12 +0900

    정수 제곱근 (이렇게 푸는건가??)

---
+++ 5일차 이분 탐색/정수_제곱근.cpp
@@ -0,0 +1,15 @@
+// https://www.acmicpc.net/problem/2417
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    double n;
+
+    scanf(" %lf", &n);
+    
+    printf("%.0lf", ceil(sqrt(n)));
+
+    return 0;
+}
